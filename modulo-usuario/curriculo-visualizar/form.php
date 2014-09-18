c5b176bb3517190b5c8c5e81288e150b8d5e6dfc
isrodrigues <ivan_feedback@hotmail.com>
2014-09-18 12:38:08 -0300

    Update form.php

--- modulo-usuario/curriculo-visualizar/form.php
+++ modulo-usuario/curriculo-visualizar/form.php
@@ -1,1 +1,26 @@
-asdfsafsaf
+<h2><i>Dados Pessoais</i></h2>
+
+    <div>Nome: <?=></div>
+    <div>Sexo: <?=></div>    
+    <div>Estado Civil: <?=></div>
+          
+    
+    <hr/>
+    
+        <h2><i>Documentos</i></h2>
+            
+            <div>Razão Social: <?=></div>
+            <div>Nome Fantasia: <?=></div>
+            <div>CPF: <?=></div>
+            <div>RG: <?=></div>
+            <div>CTPS: <?=></div>
+            <div>CNH: <?=></div>
+        
+            
+  
+    <hr/>
+    
+        <h2><i>Escolaridade</i></h2>
+            <div>Nível Escolar: <?=></div>
+            <div>Ano de conclusão: <?=></div>
+    
