df18a325c858a513c34a596663b0b6c4419085df
isrodrigues <ivan_feedback@hotmail.com>
2014-09-23 11:51:33 -0300

    Desenvolvimento do conteúdo de vagas
    Inseri o maior número de informações para vagas disponíveis.

--- modulo-usuario/vagas-chamadas/form.php
+++ modulo-usuario/vagas-chamadas/form.php
@@ -1,1 +1,7 @@
-asdfsafsaf
+<h2><legend>INserção de vagas</legend></h2>
+
+  <div>Nome da empresa: <?=></div>
+  <div>Quantidade de vagas disponibilizadas: <?=></div>
+  <div>Cargo: <?=></div>
+  <div>Salário: <?=></div>
+  <div>Requisitos mínimos:</div>
